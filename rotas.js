8e82c2140af9218ff99f9bf371b13f2542c89a94
Ramão <ramao_tiago_tiburski@hotmail.com>
2023-04-10 21:36:02 -0300

    Rotas adicionadas

--- rotas.js
+++ rotas.js
@@ -2,6 +2,21 @@ const express = require("express")
 
 const routes = express.Router()
 
+let lista = [ {"id":1,"nome":"João","idade":20},
+              {"id":2,"nome":"Maria","idade":25},
+              {"id":3,"nome":"Paula","idade":35}]
 
+routes.get("/",(req,res)=>{
+    res.status(200).json(lista)
+})
+
+routes.get("/:id",(req,res)=>{
+    res.status(200).json(lista[req.params.id-1])
+})
+
+routes.delete("/:id",(req,res)=>{
+    lista.splice(req.params.id-1,1)
+    res.status(200).redirect("/")
+})
 
 module.exports = routes
